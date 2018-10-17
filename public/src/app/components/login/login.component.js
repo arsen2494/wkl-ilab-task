ffab841d9bae55fb818292a904b433d78774a843
Arsen Adamyan <arsen_adamyan1@epam.com>
2018-10-17 18:04:09 +0400

    Login form async validation

--- public/src/app/components/login/login.component.js
+++ public/src/app/components/login/login.component.js
@@ -9,25 +9,29 @@
             $scope.onSubmit = onSubmit;
             $scope.inputHasError = inputHasError;
 
-            function onSubmit() {
+            function onSubmit(form) {
                 const {email, password} = $scope;
                 const credentials = {
                     email,
                     password
                 };
 
-                // TODO validate form
                 AuthService
                     .login(credentials)
                     .then(function (data) {
                         if (data.status === 200 && data.data.success) {
                             $location.path('/dashboard');
                         }
+                    })
+                    .catch(reason => {
+                        if (reason.status === 400 && !reason.data.success) {
+                            form.email.$error.wrong = true;
+                        }
                     });
             }
 
             function inputHasError(form, fieldName) {
-                if (form[fieldName].$invalid && form[fieldName].$touched) {
+                if (form[fieldName].$invalid && form[fieldName].$touched || fieldName === 'email' && form.email.$error.wrong) {
                     return {
                         'is-invalid': true
                     }
